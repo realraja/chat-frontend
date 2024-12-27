e8452cc807cbe2b71f5d81e9da532a076b5b86c4
realraja <146420691+realraja@users.noreply.github.com>
2024-12-27 20:16:56 +0530

    server changed

--- src/constants/config.js
+++ src/constants/config.js
@@ -6,8 +6,8 @@ export const config = {
       "Content-Type": "multipart/form-data"
     }
   }
-// export const server = 'https://chat-backend-134a.onrender.com/v1/api'
-export const server = 'http://localhost:4000/v1/api'
+export const server = 'https://chat-backend-134a.onrender.com/v1/api'
+// export const server = 'http://localhost:4000/v1/api'
 
 
 export const socketServer = 'https://chat-backend-134a.onrender.com'
